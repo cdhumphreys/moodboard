aa7ef8aa45b8da21a890e25bb26115b6d348c7cd
carlhumphreys <carl.humphreys@quotevineapp.com>
2020-07-13 16:41:12 +0100

    initial App setup

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,42 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import { ThemeProvider, createGlobalStyle, css } from "styled-components";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+import "./App.css";
+
+import UIDashboard from "./pages/UIDashboard";
+import Home from "./pages/Home";
+
+import Nav from "./components/Nav";
+
+import { darkTheme } from "./Themes";
+
+const GlobalStyles = createGlobalStyle`
+${(props) =>
+  props.theme &&
+  css`
+    body,
+    html {
+      background-color: ${props.theme.primaryBackgroundColour};
+    }
+  `}
+`;
 
 function App() {
   return (
-    <div>
-      <h1>Hello World</h1>
-    </div>
+    <BrowserRouter>
+      <ThemeProvider theme={darkTheme}>
+        <GlobalStyles />
+        <Nav />
+        <Switch>
+          <Route path="/ui-dashboard">
+            <UIDashboard />
+          </Route>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
+      </ThemeProvider>
+    </BrowserRouter>
   );
 }
 
